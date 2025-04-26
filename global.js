bb0071fe08f9b98df90143950effeedcef19b946
Kevin Chen <108315438+kevinkchen1@users.noreply.github.com>
2025-04-25 23:35:58 -0700

    lab4 changes

--- global.js
+++ global.js
@@ -92,3 +92,52 @@ function initializePage() {
 }
 
 initializePage();
+
+export async function fetchJSON(url) {
+    try {
+      // Fetch the JSON file from the given URL
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Failed to fetch projects: ${response.statusText}`);
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Error fetching or parsing JSON data:', error);
+    }
+  }
+
+export function renderProjects(project, containerElement, headingLevel = 'h2') {
+    // Your code will go here
+    if (!containerElement) {
+        console.error('Invalid container element');
+        return;
+    }
+
+    containerElement.innerHTML = '';
+
+    const validHeadings = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
+    if (!validHeadings.includes(headingLevel)) {
+        console.warn(`Invalid heading level "${headingLevel}". Falling back to "h2".`);
+        headingLevel = 'h2';
+    }
+
+    for (const proj of project) {
+        const article = document.createElement('article');
+
+    
+        article.innerHTML = `
+            <h3>${proj.title}</h3>
+            <img src="${proj.image}" alt="${proj.title}">
+            <p>${proj.description}</p>`;
+
+        containerElement.appendChild(article);
+    }
+
+  }
+  
+export async function fetchGitHubData(username) {
+    // return statement here
+    return fetchJSON(`https://api.github.com/users/${username}`);
+}
+  
